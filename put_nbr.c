6fc905eedef24ddf49f0b2b98b9f1249bbe835e3
Xavier Zanuy <xavierzanuy@MacBook-Air-de-Xavier.local>
2021-12-05 22:04:37 +0100

    c00

---
+++ put_nbr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void put_char(char c)
+{
+        write(1,&c,1);
+}
+
+void put_nbr(int);
+
+void main()
+{
+        put_nbr('42');;
+}
+
+void put_nbr(int nb)
+{
+	int tab[128];
+ 	-128 127
+	
+	put_char(nlsb);
+}	
+
